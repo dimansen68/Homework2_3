9722f47c9367add65e374085bfbca3528daaedc9
dimansen68 <dimansen15@inbox.ru>
2020-12-11 16:35:54 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double sec = 5000;
+        double hours = sec / 3600;
+        System.out.println("В " +  sec + " секунд(-ах) содержится " + hours + " часов");
+    }
+}
